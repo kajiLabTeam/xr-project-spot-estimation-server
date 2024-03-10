b7ec700fa5e76f66a64dbcdb192d8c5b7d32bf91
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-10 17:26:15 +0900

    add : リポジトリの定義

--- src/infrastructure/repository/fp_model_repository.py
+++ src/infrastructure/repository/fp_model_repository.py
@@ -1,35 +1,81 @@
 from typing import Any
 
-import psycopg
-from psycopg.rows import TupleRow
+from psycopg2.extensions import connection
 
-from domain.model.fp_model.aggregate import FpModelAggregate
-from domain.model.spot.spot_aggregate_id import SpotAggregateId
+from domain.models.fp_model.aggregate import FpModelAggregate
+from domain.models.spot.spot_id import SpotAggregateId
 from domain.repository_impl.fp_model_repository_impl import \
     FpModelRepositoryImpl
+from infrastructure.error.infrastructure_error import (InfrastructureError,
+                                                       InfrastructureErrorType)
+from infrastructure.gateway.fp_model_gateway import FpModelGateway
+
+fp_model_gateway = FpModelGateway()
 
 
-# TODO : このクラスを実装する
 class FpModelRepository(FpModelRepositoryImpl):
     def find_for_spot_id(
         self,
         s3: Any,
-        conn: psycopg.Connection[TupleRow],
+        conn: connection,
         spot_id: SpotAggregateId,
     ) -> FpModelAggregate:
-        return FpModelAggregate(
-            b"fp_model_file",
-            "extension",
-        )
+        with conn as conn:
+            fp_model_record = fp_model_gateway.find_by_spot_id(
+                conn=conn, spot_id=spot_id.get_id_of_private_value()
+            )
+            if fp_model_record is None:
+                raise InfrastructureError(
+                    InfrastructureErrorType.FP_MODEL_IS_NOT_FOUND,
+                    "Failed to find fp model",
+                )
+
+            # minioに登録されたファイルを取得するためのファイル名を宣言
+            key = (
+                spot_id.get_id_of_private_value()
+                + "."
+                + fp_model_record.get_extension_of_private_value()
+            )
+            # minioからファイルを取得
+            fp_model = fp_model_gateway.download(s3=s3, key=key)
+
+            return FpModelAggregate(
+                fp_model_file=fp_model,
+                extension=fp_model_record.get_extension_of_private_value(),
+            )
 
     def save(
         self,
         s3: Any,
-        conn: psycopg.Connection[TupleRow],
+        conn: connection,
         spot_id: SpotAggregateId,
         fp_model: FpModelAggregate,
     ) -> FpModelAggregate:
-        return FpModelAggregate(
-            b"fp_model_file",
-            "extension",
-        )
+        with conn as conn:
+            # FPモデルをDBに保存
+            fp_model_insert_result = fp_model_gateway.save(
+                conn=conn,
+                fp_model_id=fp_model.get_id_of_private_value().get_id_of_private_value(),
+                extension=fp_model.get_extension_of_private_value(),
+                spot_id=spot_id.get_id_of_private_value(),
+            )
+            if fp_model_insert_result is None:
+                raise Exception("Failed to save raw data")
+
+            # minioにアップロードするためのファイル名を宣言
+            key = (
+                spot_id.get_id_of_private_value()
+                + "."
+                + fp_model.get_extension_of_private_value()
+            )
+            # minioにファイルをアップロード
+            fp_model_upload_result = fp_model_gateway.upload(
+                s3=s3,
+                key=key,
+                fp_model_file=fp_model.get_fp_model_of_private_value(),
+            )
+
+            return FpModelAggregate(
+                fp_model_file=fp_model_upload_result,
+                extension=fp_model.get_extension_of_private_value(),
+            )
--- src/infrastructure/repository/transmitter_repository.py
+++ src/infrastructure/repository/transmitter_repository.py
@@ -1,56 +1,85 @@
 from typing import List
 
-import psycopg
-from psycopg.rows import TupleRow
+from psycopg2.extensions import connection
 
-from domain.model.spot.spot_aggregate_id import SpotAggregateId
-from domain.model.transmitter.aggregate import TransmitterAggregate
-from domain.model.transmitter.ble import Ble
-from domain.model.transmitter.wifi import Wifi
+from domain.models.spot.spot_id import SpotAggregateId
+from domain.models.transmitter.aggregate import (
+    TransmitterAggregate, TransmitterAggregateInfrastructureFactory)
+from domain.models.transmitter.ble import Ble
+from domain.models.transmitter.wifi import Wifi
 from domain.repository_impl.transmitter_repository_impl import \
     TransmitterRepositoryImpl
+from infrastructure.error.infrastructure_error import (InfrastructureError,
+                                                       InfrastructureErrorType)
+from infrastructure.gateway.transmitter_gateway import TransmitterGateway
+from infrastructure.record.transmitter_record import TransmitterRecordFactory
+
+transmitter_gateway = TransmitterGateway()
 
 
-# TODO : このクラスを実装する
 class TransmitterRepository(TransmitterRepositoryImpl):
     def find_for_spot_id(
         self,
-        conn: psycopg.Connection[TupleRow],
+        conn: connection,
         spot_id: SpotAggregateId,
     ) -> TransmitterAggregate:
-        return TransmitterAggregate(
-            [
+        with conn as conn:
+            transmitter_record = transmitter_gateway.find_by_spot_id(
+                conn=conn,
+                spot_id=spot_id.get_id_of_private_value(),
+            )
+            if transmitter_record is None:
+                raise InfrastructureError(
+                    InfrastructureErrorType.TRANSMITTER_IS_NOT_FOUND,
+                    "Failed to find transmitter",
+                )
+
+            ble_collection: List[Ble] = [
                 Ble(
-                    "ble_ssid",
-                    -90,
-                ),
-            ],
-            [
+                    ssid=ble_record.get_ssid_of_private_value(),
+                    name=ble_record.get_name_of_private_value(),
+                    rssi=10,
+                )
+                for ble_record in transmitter_record.get_ble_record_collection_of_private_value()
+            ]
+            wifi_collection: List[Wifi] = [
                 Wifi(
-                    "wifi_ssid",
-                    -90,
-                ),
-            ],
-        )
+                    name=wifi_record.get_name_of_private_value(),
+                    ssid=wifi_record.get_ssid_of_private_value(),
+                    mac_address=wifi_record.get_mac_address_of_private_value(),
+                    rssi=wifi_record.get_rssi_of_private_value(),
+                )
+                for wifi_record in transmitter_record.get_wifi_record_collection_of_private_value()
+            ]
+
+            return TransmitterAggregateInfrastructureFactory.create(
+                ble_collection=ble_collection,
+                wifi_collection=wifi_collection,
+            )
 
     def save(
         self,
-        conn: psycopg.Connection[TupleRow],
+        conn: connection,
         spot_id: SpotAggregateId,
-        ble_collection: List[Ble],
-        wifi_collection: List[Wifi],
+        transmitter: TransmitterAggregate,
     ) -> TransmitterAggregate:
-        return TransmitterAggregate(
-            [
-                Ble(
-                    "ble_ssid",
-                    -90,
-                ),
-            ],
-            [
-                Wifi(
-                    "wifi_ssid",
-                    -90,
-                ),
-            ],
-        )
+        with conn as conn:
+            transmitter_record = TransmitterRecordFactory.create(
+                transmitter.get_ble_collection_of_private_value(),
+                transmitter.get_wifi_collection_of_private_value(),
+            )
+
+            transmitter_insert_result = transmitter_gateway.save(
+                conn=conn,
+                spot_id=spot_id.get_id_of_private_value(),
+                ble_collection=transmitter_record.get_ble_record_collection_of_private_value(),
+                wifi_collection=transmitter_record.get_wifi_record_collection_of_private_value(),
+            )
+            if transmitter_insert_result is None:
+                raise InfrastructureError(
+                    InfrastructureErrorType.TRANSMITTER_INSERT_ERROR,
+                    "Failed to save transmitter",
+                )
+
+            # TODO: 保存したデータを取得して返す
+            return transmitter
