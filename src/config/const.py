10a0cd0d24f72df4aaf3d2d6dea4142d6873c1b5
Yusei Mizutani <yusei4520030401@gmail.com>
2024-03-28 21:21:50 +0900

    fix : 減衰関数を適用

--- src/config/const.py
+++ src/config/const.py
@@ -2,20 +2,21 @@ from utils.ulid import generate_ulid
 
 APPLICATION_BUCKET_NAME: str = "applications"
 
-FP_MODEL_BUCKET_NAME: str = "fp-models"
-FP_MODEL_TEMPORARY_SAVING_PATH = "./" + str(generate_ulid()) + ".csv"
-FP_MODEL_STD_DEV_THRESHOLD: float = 1.0
-FP_MODEL_LOSS_FUNCTION_VALUE_THRESHOLD: float = 160.0
-FP_MODEL_EXTENSION: str = "csv"
-
-RAW_DATA_EXTENSION: str = "csv"
-RAW_DATA_FILE_BUCKET_NAME: str = "raw-data"
-
 TRANSMITTER_RSSI_THRESHOLD: int = -80
 TRANSMITTER_THRESHOLD_NUMBER: int = 2
 TRANSMITTER_ADDRESS_NUMBER_THRESHOLD: int = 4
 TRANSMITTER_COINCIDENT_RATIO_THRESHOLD: float = 0.5
 TRANSMITTER_NUMBER_DIFFERENCE_THRESHOLD: int = 1000000
+
+RAW_DATA_EXTENSION: str = "csv"
+RAW_DATA_FILE_BUCKET_NAME: str = "raw-data"
+
+
+FP_MODEL_EXTENSION: str = "csv"
+FP_MODEL_BUCKET_NAME: str = "fp-models"
+FP_MODEL_STD_DEV_THRESHOLD: float = 1.0
+FP_MODEL_LOSS_FUNCTION_VALUE_THRESHOLD: float = 1e-11
+FP_MODEL_TEMPORARY_SAVING_PATH = "./" + str(generate_ulid()) + ".csv"
 
 
 # 確率密度関数を生成するにあたり分母が0になった場合の値
--- src/domain/models/fp_model/normal_distribution_comparator.py
+++ src/domain/models/fp_model/normal_distribution_comparator.py
@@ -33,6 +33,13 @@ class NormalDistributionComparator:
 
         return np.maximum(result, EPSILON)
 
+    def __decay_function_of_rssi(self, rssi: float) -> float:
+        """
+        RSSI の減衰関数を適用した時の RSSI の信用度を計算
+        """
+        weight = 10 ** ((rssi + 5) / 3)
+        return weight
+
     def __kl_divergence(
         self,
         p_mean: float,
@@ -40,6 +47,9 @@ class NormalDistributionComparator:
         q_mean: float,
         q_std: float,
     ) -> float:
+        """
+        KLダイバージェンスの計算
+        """
         # KLダイバージェンスの被積分関数
         integrand = lambda x: self.__normal_pdf(x, p_mean, p_std) * np.log(  # type: ignore
             self.__normal_pdf(x, p_mean, p_std) / self.__normal_pdf(x, q_mean, q_std)
@@ -52,11 +62,14 @@ class NormalDistributionComparator:
         return kl_div  # type: ignore
 
     def calculate_difference_of_normal_distribution(self) -> float:
-        kl_div = 0
-        for p_mean_list, p_std_list, q_mean_list, q_std_list in zip(
+        """
+        正規分布の差分を計算
+        """
+        normal_distribution = 0
+        for p_mean, p_std, q_mean, q_std in zip(
             self.__p_mean_list, self.__p_std_list, self.__q_mean_list, self.__q_std_list
         ):
-            kl_div += self.__kl_divergence(
-                p_mean_list, p_std_list, q_mean_list, q_std_list
-            )
-        return kl_div
+            kl_div = self.__kl_divergence(p_mean, p_std, q_mean, q_std)
+            # RSSI の減衰関数を適用した上で KLダイバージェンスを重み付け
+            normal_distribution += self.__decay_function_of_rssi(p_mean) * kl_div
+        return normal_distribution
